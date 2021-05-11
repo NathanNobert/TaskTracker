0fd9cf838227292a7c7a9ca57d68fdc1e7ae5567
NathanNobert <nobert@ualberta.ca>
2021-05-10 19:11:41 -0600

    updated db schema 01

--- sql/schema/01_schema_changes.sql
+++ sql/schema/01_schema_changes.sql
@@ -1,6 +1,26 @@
-CREATE TABLE TTR_TASK_LIST (
-    TTR_ID int IDENTITY(1,1),
-    TTR_CODE varchar(20),
-    TTR_DESC varchar(max),
-    TTR_ACTIVE bit
-)
+use TaskTrackerDev
+
+BEGIN TRY
+    BEGIN TRANSACTION
+
+    --drop table TTR_TASKS
+    CREATE TABLE TTR_TASKS (
+        TTS_ID int IDENTITY(1,1),
+        TTS_CODE varchar(20),
+        TTS_DESC varchar(max),
+        TTS_ACTIVE bit
+    )
+    insert into TTR_TASKS values ('task1', 'tester task', 1)
+
+	COMMIT TRANSACTION
+
+PRINT 'Database update completed successfully.'
+END TRY
+
+BEGIN CATCH
+	PRINT '
+	!! Database update failed: ' + ERROR_MESSAGE()
+	ROLLBACK TRANSACTION
+	PRINT '
+	Rolled Back'
+END CATCH
