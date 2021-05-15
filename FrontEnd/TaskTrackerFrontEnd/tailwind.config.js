c6e12552f818701cdfec1d94ae2a735b313ec85a
NathanNobert <nobert@ualberta.ca>
2021-05-15 11:16:02 -0600

    Add base tailwind styles and test

--- FrontEnd/TaskTrackerFrontEnd/tailwind.config.js
+++ FrontEnd/TaskTrackerFrontEnd/tailwind.config.js
@@ -1,15 +1,27 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: [
     './resources/**/*.blade.php',
     './resources/**/*.js',
     './resources/**/*.vue',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      })
+    })
+  ],
 }
